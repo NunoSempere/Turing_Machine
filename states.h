8ddb79ebe803ad392231f1d5eee33f5566f68758
Nuño Sempere <nuno.sempere@gmail.com>
2018-04-06 21:47:55 +0200

    Delete states.h

--- states.h
+++
@@ -1,6 +0,0 @@
-#ifndef __STATES_H_
-#define __STATES_H_
-
-void carry_out_step(int *symbol, int *state, int *movement); // This is the only function I'm going to need to use outside of states.c
-
-#endif
